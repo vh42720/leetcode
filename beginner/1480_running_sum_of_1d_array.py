f6bb12382eec89426c135b233f3b77a09da20ccb
Vinh <hangvinh94@gmail.com>
2023-06-14 09:50:51 -0700

    first commit

---
+++ beginner/1480_running_sum_of_1d_array.py
@@ -0,0 +1,13 @@
+"""
+Running Sum of 1d Array
+-----------------------
+https://leetcode.com/problems/running-sum-of-1d-array/
+"""
+
+
+def runningSum(self, nums: List[int]) -> List[int]:
+    pass
+
+
+if __name__ == 'main':
+    runningSum()
